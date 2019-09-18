7b977f45c2f83d3d316be683d7f9b109370f2691
James Opara <jamesopara@Jamess-MacBook-Pro.local>
2019-09-18 16:27:18 -0500

    more javascript

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
 //cards
-const brandCards = ["Tylenol", "Advil", "Mucinex", "Robitussin", "Benadryl"];
+const brandCards = ["Tylenol", "Advil", "Mucinex", "Robitussin", "Benadryl", ];
 
 const genCards1 = "Acetaminophen";
 const genCards2 = "Ibuprofen";
 const genCards3 = "Guaifenesin";
 const genCards4 = "Dextromethorphan";
 const genCards5 = "Diphenhydramine";
+
 //score
-let score = ``;
+let score = 0;
 document.querySelector("#showScore").innerHTML = `${score} Points`;
 //click
-var clickedCard = "";
+var chosenCard = "";
 var questionCard = brandCards[0];
 //if clickedCard = the proper questionCard, background color green. If wrong, red for 1 second, then back to white.
 
+function clickACard (evt) {
+    if (evt.target.querySelector("#a1") && brandCards[0]==="Tylenol") {
+        evt.target.style.backgroundColor = rgba(0, 0, 255, 0.4);
+        brandCards.shift();
+        score += 1;
+} else if (evt.target.querySelector("#a2") && brandCards[0]==="Advil") {
+    evt.target.style.backgroundColor = rgba(0, 0, 255, 0.4);
+    brandCards.shift();
+    score += 1;
+} else if (evt.target.querySelector("#a3") && brandCards[0]==="Mucinex") {
+    evt.target.style.backgroundColor = rgba(0, 0, 255, 0.4);
+    brandCards.shift();
+    score +=1;
+} else if (evt.target.querySelector("#a4") && brandCards[0]==="Robitussin") {
+    evt.target.style.backgroundColor = rgba(0, 0, 255, 0.4);
+    brandCards.shift();
+    score += 1;
+} else if (evt.target.querySelector("#a5") && brandCards[0]==="Benadryl") {
+    evt.target.style.backgroundColor = rgba(0, 0, 255, 0.4);
+    brandCards.shift();
+    score += 1;
 //Winning the game
 if (score === '5') {
-    //You Win modal pops up
+    //You Win modal pops up but alert for now
+    alert("You Win!");
 }
